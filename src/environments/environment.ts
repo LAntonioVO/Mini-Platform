a3575c0577346a705446cc43929b713aadce2c4f
Antonio Vera <veraortegaantonio@gmail.com>
2020-10-07 11:02:54 -0500

    initial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,22 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+
+
+
+
 export const environment = {
-  production: false
+  production: false,
+  // Your web app's Firebase configuration
+  firebaseConfig:{
+    apiKey: "AIzaSyCHDC7fRVGeCTN0ObegygwqIyY-bv_LFlo",
+    authDomain: "bts-mini-platform.firebaseapp.com",
+    databaseURL: "https://bts-mini-platform.firebaseio.com",
+    projectId: "bts-mini-platform",
+    storageBucket: "bts-mini-platform.appspot.com",
+    messagingSenderId: "496973552475",
+    appId: "1:496973552475:web:f58f75107a2357135a187e"
+  }
 };
 
 /*
