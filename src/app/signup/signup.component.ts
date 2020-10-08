6eb18830895038f58a8fbdc620ff535740e52085
Antonio Vera <veraortegaantonio@gmail.com>
2020-10-08 12:44:32 -0500

    create signup logic

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  signupForm: FormGroup = new FormGroup({
+    firstName: new FormControl('', [Validators.required]),
+    lastName: new FormControl('', [Validators.required]),
+    dof: new FormControl(''),
+    email: new FormControl('', [Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$"), Validators.required]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    confirmPassword: new FormControl('', [Validators.required, Validators.minLength(6)])
+  });
+
+  constructor(private auth: AuthService) { }
 
   ngOnInit(): void {
   }
 
+  submit(): void {
+    if (this.signupForm.valid) {
+      if (this.signupForm.get('password').value === this.signupForm.get('confirmPassword').value) {
+        this.auth.signUp(this.signupForm.get('email').value, this.signupForm.get('password').value)
+          .then(response => console.log("usuario creado" + response))
+          .catch(err => console.error("error" + err));
+      }
+    }
+  }
 }
