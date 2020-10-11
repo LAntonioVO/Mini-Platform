8fc499545aadd06d136e2ca8246d4a8a54e28c40
Antonio Vera <veraortegaantonio@gmail.com>
2020-10-11 16:04:31 -0500

    fix json parse error UID

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,17 +1,21 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { User } from 'firebase';
 import { Observable } from 'rxjs';
-import { connectableObservableDescriptor } from 'rxjs/internal/observable/ConnectableObservable';
 import { UserData } from '../interfaces/user.interface';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  DB = 'users';
-  index:string = JSON.parse(localStorage.getItem('user')).uid;
-  constructor(private db:AngularFirestore) { }
+  DB:string = 'users';
+  index:string;
+  constructor(private db:AngularFirestore, private auth:AuthService) {
+    const user = localStorage.getItem('user');
+    if(user!="null"){
+    this.index = JSON.parse(user).uid;
+    }
+  }
 
   create(user:UserData,UID:string):Promise<void>{
     return this.db.collection(this.DB).doc(UID).set(user);
