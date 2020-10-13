16dc3663e2583a3ed1ba7823c40544dd302474f2
Antonio Vera <veraortegaantonio@gmail.com>
2020-10-12 23:51:42 -0500

    create table for skills

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./skills.component.scss']
 })
 export class SkillsComponent implements OnInit {
+  data = [
+    { skill: "javascript", date: "12-12-1998", price: "34", description: "delicious" },
+    {skill:"javascript",date:"12-12-1998",price:"36",description:"delicious component with a mayor loremaksdjhf aslkdjhasldkfjhsa dlfkjsahdf"},
+    {skill:"javascript",date:"12-12-1998",price:"37",description:"delicious"},
+    {skill:"javascript",date:"12-12-1998",price:"38",description:"delicious"},
+    {skill:"javascript",date:"12-12-1998",price:"35",description:"delicious"},
+  ]
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  editSkill(skill){
+    console.log(skill);
+  }
 }
